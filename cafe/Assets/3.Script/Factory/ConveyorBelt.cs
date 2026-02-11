a819d916d7ccd31ea88fa2d0fbd07b2ad2bd109f
KINGBOB <9394jy@gmail.com>
2026-02-11 14:50:36 +0900

    ㅋㅁ

--- cafe/Assets/3.Script/Factory/ConveyorBelt.cs
+++ cafe/Assets/3.Script/Factory/ConveyorBelt.cs
@@ -9,10 +9,20 @@ public class ConveyorBelt : MonoBehaviour
     // 입구에 아이템이 들어오면 호출
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("FactoryItem") && _currentItem == null)
+        // 1. 트리거 자체가 작동하는지 확인
+        Debug.Log($"{other.name}이(가) 벨트에 닿았습니다.");
+
+        if (other.CompareTag("FactoryItem"))
         {
+            // 2. 태그 인식 확인
+            Debug.Log("FactoryItem 태그 인식 성공!");
+
             _currentItem = other.GetComponent<FactoryItem>();
-            _currentItem.SetTarget(endpoint, moveSpeed);
+            if (_currentItem != null)
+            {
+                _currentItem.SetTarget(endpoint, moveSpeed);
+                Debug.Log("아이템에 타겟 지점을 전달했습니다.");
+            }
         }
     }
 
